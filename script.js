edb3cc65473c00a9e2ebb5974a3ec6afe5525160
Gerry Machineroom <gerrymachineroom@gmail.com>
2022-04-26 17:50:05 -0400

    Created ramsu and ramsu families that make ramsus

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var ramsus = 0
+var automaticRamsusPerSecond = 0
 var ramsuFamilies = 0
 function startOfGame() {
   document.getElementById('ramsuFamily').style.display = 'none'
@@ -9,17 +10,27 @@ function amountOfRamsus() {
   document.getElementById('ramsu').innerHTML = 'Ramsus: ' + ramsus
 }
 amountOfRamsus()
-function spawnARamsu() {
-  document.getElementById('ramsu').innerHTML = 'Ramsus: ' + ramsus++
+function spawnRamsus(ramsusSpawned) {
+  ramsus += ramsusSpawned
+  document.getElementById('ramsu').innerHTML = 'Ramsus: ' + ramsus
 }
-spawnARamsu()
 function familyCheck() {
   if (ramsus >= 10) {
-    document.getElementById('ramsuFamily').styles.display = 'block'
+    document.getElementById('ramsuFamily').style.display = 'block'
     document.getElementById('ramsuFamily2').style.display = 'block'
   }
 }
 setInterval(familyCheck, 500)
-function spawnRamsuFamily() {
+function amountOfRamsuFamilies() {
   document.getElementById('ramsuFamily').innerHTML = 'Ramsu Families: ' + ramsuFamilies
 }
+amountOfRamsuFamilies()
+function spawnRamsuFamily() {
+  ramsuFamilies++
+  document.getElementById('ramsuFamily').innerHTML = 'Ramsu Families: ' + ramsuFamilies
+  automaticRamsusPerSecond++
+}
+function secondPassed() {
+  spawnRamsus(automaticRamsusPerSecond)
+}
+setInterval(secondPassed, 1000)
